3cc36851b9133f447d05234738ce6b51bed2d933
Ron Batts <ron.c.batts@outlook.com>
2023-08-17 19:27:28 -0500

    node version

--- src/Components/Pages/TodoPage.js
+++ src/Components/Pages/TodoPage.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import { Card } from '../Components/Card/card';
 
-
 export const TodoPage = ()=> {
+
+  const [todo, setTodo] = useState([])
+
+  useEffect(()=> {
+    fetch('/api').then(response => {
+      if(response.ok){
+        return response.json()
+      }
+    }).then(data => console.log(data))
+  },[])
+
   return(
     <>
       <card/>
